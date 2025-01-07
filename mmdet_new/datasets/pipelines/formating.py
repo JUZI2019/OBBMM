72bb469f71fe55995b0f33cd353bcbaf427f1857
JUZI2019 <1274255154@qq.com>
2025-01-07 19:50:50 +0800

    docs: update mmcv mmdet

---
+++ mmdet_new/datasets/pipelines/formating.py
@@ -0,0 +1,9 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+# flake8: noqa
+import warnings
+
+from .formatting import *
+
+warnings.warn('DeprecationWarning: mmdet_new.datasets.pipelines.formating will be '
+              'deprecated, please replace it with '
+              'mmdet_new.datasets.pipelines.formatting.')
